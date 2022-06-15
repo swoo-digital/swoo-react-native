29f8004f2044d6480f83be7f32123923359b9f83
Kevin Stubbs <kevin@pubgenius.io>
2022-06-15 18:11:51 +0900

    Switch to using styled-components/native.

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,68 +1,63 @@
 import React from 'react';
-import {StyleSheet, Text, TouchableNativeFeedback, View} from 'react-native';
+import {TouchableNativeFeedback} from 'react-native';
 import {useAppDispatch, useAppSelector} from '../../hooks';
 import {decrement, increment} from './slice';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  backgroundColor: #004761;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justifyContent: center;
+  alignItems: center;
+`;
+
+const CounterText = styled.Text`
+  color: #8cd6bd;
+  fontSize: 64px;
+  fontWeight: 700;
+  fontFamily: Inter-Bold;
+  marginTop: 104px;
+  marginBottom: 104px;
+`;
+
+const CounterButtonText = styled.Text`
+  fontSize: 24px;
+  fontWeight: 700;
+  textAlign: center;
+`;
+
+const CounterButton = styled.View`
+  backgroundColor: #ffffff;
+  padding: 16px;
+  borderRadius: 24px;
+  width: 160px;
+`;
 
 const Counter = () => {
   const count = useAppSelector(state => state.counter.value);
   const dispatch = useAppDispatch();
 
   return (
-    <View style={styles.container}>
-      <View style={styles.counterButton}>
+    <Container>
+      <CounterButton>
         <TouchableNativeFeedback
           onPress={() => dispatch(decrement())}
           data-testid="button-decrement">
-          <Text style={styles.counterButtonContent}>-1</Text>
+          <CounterButtonText>-1</CounterButtonText>
         </TouchableNativeFeedback>
-      </View>
-      <Text style={styles.counterValue} data-testid="counter-value">
-        {count}
-      </Text>
-      <View style={styles.counterButton}>
+      </CounterButton>
+      <CounterText data-testid="counter-value">{count}</CounterText>
+      <CounterButton>
         <TouchableNativeFeedback
           onPress={() => dispatch(increment())}
           data-testid="button-increment">
-          <Text style={styles.counterButtonContent}>+1</Text>
+          <CounterButtonText>+1</CounterButtonText>
         </TouchableNativeFeedback>
-      </View>
-    </View>
+      </CounterButton>
+    </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  counterValue: {
-    color: '#8cd6bd',
-    fontSize: 64,
-    fontWeight: '700',
-    fontFamily: 'Inter-Bold',
-
-    // Alternatively, this could be based on a percentage
-    // (for example the counter content could be made to take up 2/3rds of available screen space),
-    // however since nothing was specified, this simple approach to copy the distances as given in the mockup
-    // was preferable.
-    marginTop: 104,
-    marginBottom: 104,
-  },
-  counterButtonContent: {
-    fontSize: 24,
-    fontWeight: '700',
-    textAlign: 'center',
-  },
-  counterButton: {
-    backgroundColor: '#ffffff',
-    padding: 16,
-    borderRadius: 24,
-    width: 160,
-  },
-  container: {
-    backgroundColor: '#004761',
-    height: '100%',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default Counter;
