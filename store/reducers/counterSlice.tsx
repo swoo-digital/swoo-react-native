d7dab4935c05b18986cd8f45938f9d4d56c59f76
Stas Shmiriov <stas.shmiriov@pettersonapps.com>
2022-12-15 13:58:16 +0200

    Added Redux

---
+++ store/reducers/counterSlice.tsx
@@ -0,0 +1,22 @@
+import {createSlice} from '@reduxjs/toolkit';
+interface InitialState {
+  value: number;
+}
+const initialState: InitialState = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: state => {
+      state.value += 1;
+    },
+    decrement: state => {
+      state.value -= 1;
+    },
+  },
+});
+export const {increment, decrement} = counterSlice.actions;
+export default counterSlice;
