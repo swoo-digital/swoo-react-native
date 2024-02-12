1890f7bc5abd28f68c4d794effc896851332a701
Franco Fadini <franco@made2.co>
2024-02-12 20:26:05 +0100

    Add theme

--- App.tsx
+++ App.tsx
@@ -1,48 +1,19 @@
-import React, {useState} from 'react';
-import styled from 'styled-components/native';
-import {StatusBar} from 'react-native';
-import SimpleButton from './src/components/SimpleButton';
+import React from 'react';
+import {ThemeProvider} from 'styled-components/native';
+import CounterScreen from './src/CounterScreen';
 
-const Counter = styled.Text`
-  font-size: 64px;
-  padding-vertical: 80px;
-  font-weight: bold;
-  color: #8cd6bd;
-`;
-const CounterContainer = styled.View`
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-const SafeArea = styled.SafeAreaView`
-  flex: 1;
-  background-color: #004761;
-`;
+const theme = {
+  colors: {
+    primary: '#004761',
+    secondary: '#8CD6BD',
+  },
+};
 
 const App = () => {
-  // Is wrong to save this state on Redux. As it is a local state, it should be saved on the screen.
-  // Even if the feature is going to be used in other screens, it is better to use a context to share the state.
-  // Sorry I don't like Redux, I think it is a practice that is not necessary in most cases, and its harder to maintain.
-  const [currentCount, setCurrentCount] = useState(0);
-
-  const onIncrement = () => {
-    setCurrentCount(currentCount + 1);
-  };
-
-  const onDecrement = () => {
-    setCurrentCount(currentCount - 1);
-  };
-
   return (
-    <SafeArea>
-      <StatusBar barStyle="light-content" />
-      <CounterContainer>
-        <SimpleButton label="-1" onPress={onDecrement} />
-        <Counter>{currentCount}</Counter>
-        <SimpleButton label="+1" onPress={onIncrement} />
-      </CounterContainer>
-    </SafeArea>
+    <ThemeProvider theme={theme}>
+      <CounterScreen />
+    </ThemeProvider>
   );
 };
 
---
+++ src/theme.ts
@@ -0,0 +1,6 @@
+export const theme = {
+  colors: {
+    primary: '#8cd6bd',
+    secondary: '#f5f5f5',
+  },
+};
