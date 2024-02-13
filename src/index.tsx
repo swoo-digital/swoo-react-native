e4139ec3e17ba7f593e3bea2b156a80bf28b991d
mauripavan <mauriciopavan60@gmail.com>
2024-02-13 18:27:48 -0300

    feat: desing Counter screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import styled, {useTheme} from 'styled-components';
+import CounterButton from './components/CounterButton';
+import {TextBold} from './components/Typography';
+import Separator from './components/Separator';
 
 const Home = () => {
+  const {colors} = useTheme();
+
+  const handleDecrease = () => {
+    console.log('decrese');
+  };
+
+  const handleIncrease = () => {
+    console.log('increase');
+  };
   return (
-    <View>
-      <Text>Counter Screen</Text>
-    </View>
+    <Box>
+      <CounterButton text="-1" onPress={handleDecrease} />
+      <Separator size={104} />
+      <TextBold fontSize={64} color={colors.green[0]}>
+        0
+      </TextBold>
+      <Separator size={104} />
+      <CounterButton text="+1" onPress={handleIncrease} />
+    </Box>
   );
 };
 
 export default Home;
+
+const Box = styled(View)`
+  background-color: ${({theme}) => theme.colors.blue[0]};
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -3,4 +3,10 @@
 export const colors = {
   black: '#000000',
   white: '#FFFFFF',
+  blue: {
+    0: '#004761',
+  },
+  green: {
+    0: '#8CD6BD',
+  },
 };
