fe28d66b39ee97889174d08eb45e833b32d75f45
vladyslav.pyrozhenko <vladyslav.pyrozhenko@dotcode.pro>
2024-02-13 17:37:48 +0200

    wip: created custom-button component

---
+++ src/components/buttons/custom-button.component.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {ICustomButtonProps} from '../../interfaces/custom-button-props.interface';
+import styled from 'styled-components/native';
+
+const CustomButton: React.FC<ICustomButtonProps> = ({title, onPress}) => {
+    const Button = styled.Button`
+    background-color: white;
+    border-radius: 24px;
+    display: grid;
+    place-content: center;
+    color: black;
+    font-size: 24px;
+    line-height: 1.2;
+  `;
+
+    return <Button title={title} onPress={onPress} />;
+};
+
+export default CustomButton;
