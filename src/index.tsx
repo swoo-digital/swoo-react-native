d9cdb6144c6a1d5b4298dc56d522254be05ba3b4
mauripavan <mauriciopavan60@gmail.com>
2024-02-13 18:34:31 -0300

    feat: counter implemented

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import {View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 import styled, {useTheme} from 'styled-components';
 import CounterButton from './components/CounterButton';
 import {TextBold} from './components/Typography';
 import Separator from './components/Separator';
+import {RootState} from './store';
+import {decrement, increment} from './store/reducers/counterReducer';
 
 const Home = () => {
   const {colors} = useTheme();
+  const dispatch = useDispatch();
+  const counter = useSelector((state: RootState) => state.counter.value);
 
   const handleDecrease = () => {
-    console.log('decrese');
+    dispatch(decrement());
   };
 
   const handleIncrease = () => {
-    console.log('increase');
+    dispatch(increment());
   };
   return (
     <Box>
       <CounterButton text="-1" onPress={handleDecrease} />
       <Separator size={104} />
       <TextBold fontSize={64} color={colors.green[0]}>
-        0
+        {counter}
       </TextBold>
       <Separator size={104} />
       <CounterButton text="+1" onPress={handleIncrease} />
