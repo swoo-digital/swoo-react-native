0aaf9dd75f9c9439e06cf08afc8cc2554d623024
vladyslav.pyrozhenko <vladyslav.pyrozhenko@dotcode.pro>
2024-02-13 17:32:14 +0200

    wip: created a layout component

---
+++ src/components/layout/layout-component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Layout: React.FC<{children: React.ReactNode}> = ({children}) => {
+    const ContainerView = styled.SafeAreaView`
+    height: 100%;
+    background-color: #004761;
+  `;
+
+    return <ContainerView>{children}</ContainerView>;
+};
+export default Layout;
