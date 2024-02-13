b8e0610366af9267aeccd86985a4d179bc9a6d21
vladyslav.pyrozhenko <vladyslav.pyrozhenko@dotcode.pro>
2024-02-13 17:39:31 +0200

    wip: created increase and decreased ui components

---
+++ src/components/counter/decrease.component.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CustomButton from '../buttons/custom-button.component';
+
+const DecreaseComponent: React.FC<{decrease: () => void}> = ({decrease}) => {
+    return <CustomButton title={'-'} onPress={decrease} />;
+};
+
+export default DecreaseComponent;
---
+++ src/components/counter/increase.component.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CustomButton from '../buttons/custom-button.component';
+
+const IncreaseComponent: React.FC<{increase: () => void}> = ({increase}) => {
+    return <CustomButton title={'+'} onPress={increase} />;
+};
+
+export default IncreaseComponent;
