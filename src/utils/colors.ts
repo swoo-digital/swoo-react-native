8205afea5984a361d44691c3b6eabd0893d4239b
dev-talal <talaltahir7899@gmail.com>
2024-02-14 23:06:26 +0500

    colors

---
+++ src/utils/colors.ts
@@ -0,0 +1,13 @@
+interface ColorType {
+  black: string;
+  white: string;
+  green: string;
+  bg: string;
+}
+
+export const colors: ColorType = {
+  black: '#000000',
+  white: '#fff',
+  green: '#8CD6BD',
+  bg: '#004761',
+};
